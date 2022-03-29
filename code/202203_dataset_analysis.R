c366fd4ec2169066c071e280d74774ae00b9c92f
Pernat <npernat@exchange.wwu.de>
2022-03-29 18:41:58 +0200

    First version for biohackrxiv paper

---
+++ code/202203_dataset_analysis.R
@@ -0,0 +1,473 @@
+# 
+# 0. Packages
+install.packages("tidyverse")
+install.packages("ggplot2")
+install.packages("viridis")
+
+# 1. characteristics of App and Expert data ####
+# App scores and candidate lists
+library(tidyverse)
+
+isomex_250 %>% 
+  count(sampleID) %>% 
+  summarize(mean=mean(n), max=max(n), min=min(n), median=quantile(n,0.5))
+
+isomex_250 %>% 
+  select(score) %>% 
+  drop_na() %>% 
+  summarize(mean=mean(score), max=max(score), min=min(score), median=quantile(score,0.5))
+
+isomex_250 %>% 
+  select(sampleID, score) %>% 
+  drop_na() %>% 
+  group_by(sampleID) %>% 
+  summarize(mean=sum(score)/length(sampleID)) %>% 
+  summarize(mean_all=sum(mean)/length(mean))
+
+# Experts identification frequencies
+# some characteristics of expert and app identification
+# proportion of identifiable/unidentifiable
+isomex_first %>% 
+  select(species_exp) %>% 
+  mutate(spec_exp=case_when(
+    species_exp!="unidentifiable"
+    & species_exp!="noflower"
+    & species_exp!="need_id"~"identified",
+    TRUE ~ as.character(species_exp)
+  )) %>% 
+  group_by(spec_exp) %>% 
+  tally() %>% 
+  mutate(prop=n/sum(n)) %>% 
+  write.csv2(.,"output/species_expert_identification.xls")
+
+# 2. Matching species, genera and family ####
+
+# including all candidates
+# species 
+isomex_250_scores %>% 
+  filter(species_exp!="noflower" 
+         & species_exp!="need_id"
+         & species_exp!="unidentifiable") %>% 
+  mutate(agree=case_when(
+    latin_name==species_exp~"yes",
+    latin_name!=species_exp~"no"))%>% 
+  count(agree)%>% 
+  mutate(perc=n/sum(n))  
+
+# genus
+isomex_250_scores%>%  
+  filter(genus_exp!="noflower" 
+         & genus_exp!="need_id"
+         & genus_exp!="unidentifiable") %>% 
+  mutate(agree=case_when(
+    genus==genus_exp~"yes",
+    genus!=genus_exp~"no"))%>% 
+  count(agree)%>% 
+  mutate(perc=n/sum(n))  
+
+# family
+isomex_250_scores%>% 
+  filter(family_exp!="noflower" 
+         & family_exp!="need_id"
+         & family_exp!="unidentifiable") %>% 
+  mutate(agree=case_when(
+    family_exp==family~"yes",
+    family_exp!=family~"no"))%>% 
+  count(agree)%>% 
+  mutate(perc=n/sum(n))  
+
+# Including only the first candidate (scores) of the App
+# Agreement with cutting scores - descending 
+
+# species 
+isomex_first %>% 
+  filter(species_exp!="noflower" 
+         & species_exp!="need_id"
+         & species_exp!="unidentifiable") %>% 
+  mutate(agree=case_when(
+    latin_name==species_exp~"yes",
+    latin_name!=species_exp~"no"))%>% 
+  count(agree)%>% 
+  mutate(perc=n/sum(n))  
+
+# genus
+isomex_first %>%  
+  filter(genus_exp!="noflower" 
+         & genus_exp!="need_id"
+         & genus_exp!="unidentifiable") %>% 
+  mutate(agree=case_when(
+    genus==genus_exp~"yes",
+    genus!=genus_exp~"no"))%>% 
+  count(agree)%>% 
+  mutate(perc=n/sum(n))  
+
+# family
+isomex_first%>% 
+  filter(family_exp!="noflower" 
+         & family_exp!="need_id"
+         & family_exp!="unidentifiable") %>% 
+  mutate(agree=case_when(
+    family_exp==family~"yes",
+    family_exp!=family~"no"))%>% 
+  count(agree)%>% 
+  mutate(perc=n/sum(n))  
+# Agreements with cutting scores > 80%
+# species 
+isomex_first %>% 
+  filter(score>=0.80) %>%
+  filter(species_exp!="noflower" 
+         & species_exp!="need_id"
+         & species_exp!="unidentifiable") %>% 
+  mutate(agree=case_when(
+    latin_name==species_exp~"yes",
+    latin_name!=species_exp~"no"))%>% 
+  count(agree)%>% 
+  mutate(perc=n/sum(n))  
+
+# genus
+isomex_first %>%  
+  filter(score>=0.80) %>%
+  filter(genus_exp!="noflower" 
+         & genus_exp!="need_id"
+         & genus_exp!="unidentifiable") %>% 
+  mutate(agree=case_when(
+    genus==genus_exp~"yes",
+    genus!=genus_exp~"no"))%>% 
+  count(agree)%>% 
+  mutate(perc=n/sum(n))  
+
+# family
+isomex_first %>% 
+  filter(score>=0.80) %>%
+  filter(family_exp!="noflower" 
+         & family_exp!="need_id"
+         & family_exp!="unidentifiable") %>% 
+  mutate(agree=case_when(
+    family_exp==family~"yes",
+    family_exp!=family~"no"))%>% 
+  count(agree)%>% 
+  mutate(perc=n/sum(n))  
+
+
+# Agreements with cutting scores > 50%
+
+# species 
+isomex_first %>% 
+  filter(score>=0.50) %>%
+  filter(species_exp!="noflower" 
+         & species_exp!="need_id"
+         & species_exp!="unidentifiable") %>% 
+  mutate(agree=case_when(
+    latin_name==species_exp~"yes",
+    latin_name!=species_exp~"no"))%>% 
+  count(agree)%>% 
+  mutate(perc=n/sum(n))  
+
+# genus
+isomex_first %>%  
+  filter(score>=0.50) %>%
+  filter(genus_exp!="noflower" 
+         & genus_exp!="need_id"
+         & genus_exp!="unidentifiable") %>% 
+  mutate(agree=case_when(
+    genus==genus_exp~"yes",
+    genus!=genus_exp~"no"))%>% 
+  count(agree)%>% 
+  mutate(perc=n/sum(n))  
+
+# family
+isomex_first %>% 
+  filter(score>=0.50) %>%
+  filter(family_exp!="noflower" 
+         & family_exp!="need_id"
+         & family_exp!="unidentifiable") %>% 
+  mutate(agree=case_when(
+    family_exp==family~"yes",
+    family_exp!=family~"no"))%>% 
+  count(agree)%>% 
+  mutate(perc=n/sum(n)) 
+
+# Agreements with cutting scores > 30%
+
+# species 
+isomex_first %>% 
+  filter(score>=0.30) %>%
+  filter(species_exp!="noflower" 
+         & species_exp!="need_id"
+         & species_exp!="unidentifiable") %>% 
+  mutate(agree=case_when(
+    latin_name==species_exp~"yes",
+    latin_name!=species_exp~"no"))%>% 
+  count(agree)%>% 
+  mutate(perc=n/sum(n))  
+
+# genus
+isomex_first %>%  
+  filter(score>=0.30) %>%
+  filter(genus_exp!="noflower" 
+         & genus_exp!="need_id"
+         & genus_exp!="unidentifiable") %>% 
+  mutate(agree=case_when(
+    genus==genus_exp~"yes",
+    genus!=genus_exp~"no"))%>% 
+  count(agree)%>% 
+  mutate(perc=n/sum(n))  
+
+# family
+isomex_first %>% 
+  filter(score>=0.30) %>%
+  filter(family_exp!="noflower" 
+         & family_exp!="need_id"
+         & family_exp!="unidentifiable") %>% 
+  mutate(agree=case_when(
+    family_exp==family~"yes",
+    family_exp!=family~"no"))%>% 
+  count(agree)%>% 
+  mutate(perc=n/sum(n)) 
+
+# # Agreements with cutting scores <30%
+
+# species 
+isomex_first %>% 
+  filter(score<0.30) %>%
+  filter(species_exp!="noflower" 
+         & species_exp!="need_id"
+         & species_exp!="unidentifiable") %>% 
+  mutate(agree=case_when(
+    latin_name==species_exp~"yes",
+    latin_name!=species_exp~"no"))%>% 
+  count(agree)%>% 
+  mutate(perc=n/sum(n))  
+
+# Agreements with cutting scores - genus
+isomex_first %>%  
+  filter(score<0.30) %>%
+  filter(genus_exp!="noflower" 
+         & genus_exp!="need_id"
+         & genus_exp!="unidentifiable") %>% 
+  mutate(agree=case_when(
+    genus==genus_exp~"yes",
+    genus!=genus_exp~"no"))%>% 
+  count(agree)%>% 
+  mutate(perc=n/sum(n))  
+
+# Agreements with cutting scores - family
+isomex_first %>% 
+  filter(score<0.30) %>%
+  filter(family_exp!="noflower" 
+         & family_exp!="need_id"
+         & family_exp!="unidentifiable") %>% 
+  mutate(agree=case_when(
+    family_exp==family~"yes",
+    family_exp!=family~"no"))%>% 
+  count(agree)%>% 
+  mutate(perc=n/sum(n)) 
+
+# 3. Figures - matching species, genera and family ####
+library(ggplot2)
+library(viridis)
+
+# Figure matching species, threshold >0.8
+# create subset of expert data
+species_exp_first_08<-isomex_first %>% 
+  filter(score>=0.8) %>% 
+  select(species_exp) %>% 
+  filter(species_exp!="noflower" 
+         & species_exp!="need_id"
+         & species_exp!="unidentifiable") %>% 
+  group_by(species_exp) %>% 
+  tally() %>% 
+  arrange(desc(n)) %>% 
+  mutate(id_by="Expert")
+colnames(species_exp_first_08)<-c("species", "n","id_by")
+
+# create subset of app data
+species_app_first_08<-isomex_first %>% 
+  filter(score>=0.8) %>% 
+  select(latin_name) %>% 
+  group_by(latin_name) %>% 
+  tally() %>% 
+  arrange(desc(n)) %>% 
+  mutate(id_by="App")
+colnames(species_app_first_08)<-c("species", "n","id_by")
+
+# bind subsets
+species_df_first_08<-rbind(species_exp_first_08, species_app_first_08)
+head(species_df_first_08)
+
+# breaks and labels for figure
+brks_50_spec = seq(-3, 3, 1)
+lbls_50_spec = paste0(as.character(c(seq(3, 0, -1), seq(1, 3,1))))
+
+# figure
+library(ggplot2)
+library(viridis)
+tiff("./output/species_props_first_08.tiff", units="px",width = 3000,height = 2000,res = 360)
+species_df_first_08 %>% 
+  mutate(counts=ifelse(id_by=="Expert", n, -n)) %>%  
+  ggplot(., aes(x=reorder(species, counts), y=counts, fill=id_by))+
+  geom_bar(stat="identity", width=0.5)+
+  scale_y_continuous(breaks = brks_50_spec,
+                     limits=c(-3,3),
+                     labels = lbls_50_spec)+
+  coord_flip() +  # flip axes
+  theme_bw() +
+  theme(axis.ticks = element_blank(),
+        axis.text.y = element_text(size=10, face = "italic"),
+        axis.title.y = element_blank(),
+        axis.title.x = element_text(size=10),
+        axis.text.x = element_text(size=10),
+        legend.text = element_text(size=10),
+        panel.grid.minor = element_blank()) +
+  labs(y="Counts", fill="Identified by", 
+       title="Plant species by method \n(scores>0.8, first score per URL)")+
+  scale_fill_viridis(discrete=T, begin=0.1, end=0.95, labels=c("App","Experts"))        
+dev.off()
+
+
+# Figure matching genera, threshold >0.5
+
+# create subset of expert data
+genus_exp_first<-isomex_first %>% 
+  filter(score>=0.5) %>% 
+  select(genus_exp) %>% 
+  filter(genus_exp!="noflower" 
+         & genus_exp!="need_id"
+         & genus_exp!="unidentifiable") %>% 
+  group_by(genus_exp) %>% 
+  tally() %>% 
+  arrange(desc(n)) %>% 
+  mutate(id_by="Expert")
+colnames(genus_exp_first)<-c("genus", "n","id_by")
+
+# create subset of app data
+genus_app_first<-isomex_first %>% 
+  filter(score>=0.5) %>% 
+  select(genus) %>% 
+  filter(!is.na(genus))%>% 
+  group_by(genus) %>% 
+  tally() %>% 
+  arrange(desc(n)) %>% 
+  mutate(id_by="App")
+colnames(genus_app_first)<-c("genus", "n","id_by")
+
+# bind subsets
+genus_df_first<-rbind(genus_exp_first, genus_app_first)
+summary(genus_df_first)
+
+
+
+# breaks and labels for figure
+brks_50_genus = seq(-36, 36, 2)
+lbls_50_genus = paste0(as.character(c(seq(36, 0, -2), seq(2, 36,2))))
+
+# figure
+tiff("./output/genus_props_first.tiff", units="px",width = 3000,height = 2000,res = 360)
+genus_df_first %>% 
+  #filter(n>3) %>% 
+  mutate(counts=ifelse(id_by=="Expert", n, -n)) %>%  
+  ggplot(., aes(x=reorder(genus, counts), y=counts, fill=id_by))+
+  geom_bar(stat="identity", width=0.5)+
+  scale_y_continuous(breaks = brks_50_genus,
+                     #limits=c(-10,10),
+                     labels = lbls_50_genus)+
+  coord_flip() +  # flip axes
+  theme_bw() +
+  theme(axis.ticks = element_blank(),
+        axis.text.y = element_text(size=10, face="italic"),
+        axis.title.y = element_blank(),
+        axis.title.x = element_text(size=10),
+        axis.text.x = element_text(size=10),
+        legend.text = element_text(size=10),
+        panel.grid.minor = element_blank()) +
+  labs(y="Counts", fill="Identified by", 
+       title="Plant genus by method \n(scores>0.5, first score per URL)")+
+  scale_fill_viridis(discrete=T, begin=0.1, end=0.95, labels=c("App","Experts"))        
+dev.off()
+
+# Figure matching families, threshold >0.5
+family_exp_first<-isomex_first %>% 
+  filter(score>=0.5) %>% 
+  select(family_exp) %>% 
+  filter(family_exp!="noflower" 
+         & family_exp!="need_id"
+         & family_exp!="unidentifiable") %>% 
+  mutate(family_exp=case_when(
+    family_exp=="Apiaceaea"~"Apiaceae",
+    TRUE ~ as.character(family_exp)))%>% 
+  group_by(family_exp) %>% 
+  tally() %>% 
+  arrange(desc(n)) %>% 
+  mutate(id_by="Expert")
+colnames(family_exp_first)<-c("family", "n","id_by")
+
+family_app_first<-isomex_first %>% 
+  filter(score>=0.5) %>% 
+  select(family) %>% 
+  group_by(family) %>% 
+  tally() %>% 
+  arrange(desc(n)) %>% 
+  mutate(id_by="App")
+colnames(family_app_first)<-c("family", "n","id_by")
+
+family_df_first<-rbind(family_exp_first, family_app_first)
+head(family_df_first)
+
+# breaks and labels for figure
+brks_50_family = seq(-10, 10, 1)
+lbls_50_family = paste0(as.character(c(seq(10, 0, -1), seq(1, 10,1))))
+
+# figure
+tiff("./output/family_props_first.tiff", units="px",width = 3000,height = 2000,res = 360)
+family_df_first %>% 
+  mutate(counts=ifelse(id_by=="Expert", n, -n)) %>%  
+  ggplot(., aes(x=reorder(family, counts), y=counts, fill=id_by))+
+  geom_bar(stat="identity", width=0.5)+
+  scale_y_continuous(breaks = brks_50_family,
+                     limits=c(-10,10),
+                     labels=lbls_50_family)+
+  coord_flip() +  # flip axes
+  theme_bw() +
+  theme(axis.ticks = element_blank(),
+        axis.text.y = element_text(size=10, face="italic"),
+        axis.title.y = element_blank(),
+        axis.title.x = element_text(size=10),
+        axis.text.x = element_text(size=10),
+        legend.text = element_text(size=10),
+        panel.grid.minor = element_blank()) +
+  labs(y="Counts", fill="Identified by", 
+       title="Plant family by method \n(scores>0.5, first score per URL)")+
+  scale_fill_viridis(discrete=T, begin=0.1, end=0.95, labels=c("App","Experts"))        
+dev.off()
+
+
+# 4. Species list ####
+
+# from expert data
+isomex_first %>% 
+  select(species_exp) %>% 
+  count(species_exp) %>% 
+  arrange(desc(n)) %>% 
+  write.csv2("output/specieslist_experts.xls")
+
+# from app data
+isomex_first %>% 
+  filter(score >=0.5) %>% 
+  select(latin_name) %>% 
+  count(latin_name) %>% 
+  arrange(desc(n)) %>% 
+  write.csv2("output/specieslist_app.xls")
+
+# from Romania
+isomex_rom <-read_delim("data/Isomex_rom.csv", delim = ";", escape_double = FALSE, trim_ws = TRUE)
+isomex_rom<-isomex_rom[,c(1:4)]
+colnames(isomex_rom)<-c("id", "source", "species","interaction")
+str(isomex_rom)
+
+# some investigation
+specmatch_rom_app<-unique(isomex_250[which(isomex_250$latin_name == isomex_rom$species),]$latin_name)
+specmatch_rom_experts<-unique(isomex_250[which(isomex_250$species_exp == isomex_rom$species),]$species_exp)
+unique(isomex_rom$species)
+
+# Romanian species and species identification by the App were added manually to the
+# expert species list
---
+++ code/202203_dataset_preparation.R
@@ -0,0 +1,125 @@
+# Dataset preparations ####
+
+# 0. Packages ####
+install.packages("rinat")
+install.package("zoo")
+install.packages("httr")
+install.packages("tidyverse")
+install.packages("cli")
+devtools::install_github(repo = 'BiologicalRecordsCentre/plantnet')
+install.packages("stringr")
+install.pakcages("taxize")
+library(rinat)
+library(zoo)
+library(httr)
+library(tidyverse)
+library(cli)
+library(plantnet)
+library(stringr)
+library(taxize)
+
+# 1. retrieving images from iNaturalist ####
+inat<-get_inat_obs(taxon_name="Isodontia mexicana", maxresults = 10000) 
+# get iNaturalist observation of I. mexicana, but it only works for one year at a time
+# adapt function to work for more than one year
+
+years<-data.frame(year=c(2007:2021)) # define the years observations should be downloaded
+inat_fun<-function(x){get_inat_obs_nostop(taxon_name = "Isodontia mexicana", meta=F,
+                                          maxresults = 10000, year=x, quality="research")}
+# *I adapted the function so that it won't stop when a result is zero for a year
+Isodontia<-apply(years, 1, inat_fun) # loop over years
+names(Isodontia)<-as.character(c(2007:2021)) # list of observations, years are list elements
+Isodontia_df<-bind_rows(Isodontia) # create a dataframe
+# check dataframe
+dim(Isodontia_df)
+head(Isodontia_df$taxon_id)
+names(Isodontia_df)
+iso_df<-Isodontia_df[,c(1,5,6,9,10,19,22,37)] # subset of interesting variables to keep
+iso_df$SampleID<-c(1:nrow(iso_df)) # create ID number for merging datasets later
+img_iso<-data.frame(imageURL=iso_df$image_url) # create a dataframe with all image URLs
+# iso_df was exported for the Expert identification
+
+# 2. Feed iNaturalist image URLs in PlantNet App and prepare dataset for merging with expert data ####
+
+# create function for plantNet App to run over more than one URL
+# get the key at https://my.plantnet.org/usage
+fun_plantnet_all_simp<-function(x){identify(key, imageURL=x, no_reject = 'true',
+                                            simplify="TRUE")}
+iso_all<-apply(data.frame(img_iso[,1]), 1, fun_plantnet_all_simp) # loop functions over all image URLs
+
+# save results
+iso_all_id<-mapply(cbind, iso_all, "SampleID"=1:length(iso_all), SIMPLIFY=F)
+notfound<-as.vector(which(sapply(iso_all_id, function(x) is.element("Species Not Found", x))))
+iso_all_id_cc<-iso_all_id[-c(notfound)]
+iso_all_df<-data.frame(do.call(rbind, iso_all_id_cc))
+
+score_iso<-data.frame(score=unlist(isop_all_df))
+latin_name_iso<-data.frame(latin_name=unlist(isop_all_df[,2]))
+common_name_iso<-data.frame(common_name=unlist(isop_all_df[,3]))
+sampleID_iso<-data.frame(sampleID=unlist(isop_all_df[,4]))
+iso_save<-data.frame(score=score_isop, latin_name=latin_name_isop, 
+                      common_name= common_name_isop, sampleID=sampleID_isop)
+str(iso_save)
+# write.csv2(iso_save, "iso_save.xls")
+# further work with imported dataframe iso_save
+
+# pick out the first 250 samples
+isomex_250_test<-iso_save[c(1:10840),] # strangest thing that I could not filter
+# by the sampleID but needed to look up the last row for sampleID 250 in the dataset
+
+# get genus name from package stringr
+
+library(stringr)
+isomex_250_test$genus<-word(isomex_250_test$latin_name, 1) # gets the first word aka. the genus 
+table(isomex_250_test$genus)
+
+# get family name from package taxize 
+# test with plantminer
+plantminer<-plantminer(isomex_250_test$latin_name, from="tpl")
+head(plantminer)
+str(plantminer)
+isomex_250_test$family<-plantminer$family
+str(isomex_250_test)
+isomex_250_test$score<-as.numeric(isomex_250_test$score)
+
+# 3. Merge expert and plantNet dataset ####
+
+# isomex_expert was imported after expert verification of URLs
+# not run: readxls...
+isomex_experts_250<-isomex_experts %>% 
+  filter(SampleID<=250) %>% 
+  rename(sampleID=SampleID)
+isomex_250<-left_join(isomex_250_test, isomex_experts_250, by="sampleID")
+
+
+# check important variables of proper format and correctness (e.g. spelling mistakes, dislocations)
+isomex_250$score<-as.numeric(isomex_250$score)
+table(isomex_250$species_exp)
+table(isomex_250$family_exp) # some wrong spellings
+isomex_250$family_exp<-gsub("Apiaceaea","Apiaceae",isomex_250$family_exp)
+isomex_250$family_exp<-gsub("unindetifiable","unidentifiable",isomex_250$family_exp)
+table(isomex_250$genus_exp) # some wrong spellings
+table(isomex_250$genus)
+isomex_250$genus_exp<-gsub("unindetifiable","unidentifiable",isomex_250$genus_exp)
+isomex_250$genus_exp<-gsub("Euonimus","Euonymus",isomex_250$genus_exp)
+which(isomex_250$species_exp=="noplant")
+table(isomex_250$species_exp)
+isomex_250$species_exp<-gsub("noplant","noflower",isomex_250$species_exp)
+which(isomex_250$species_exp=="Cirsium arvensae")
+isomex_250$species_exp<-gsub("Cirsium arvensae","Cirsium arvense",isomex_250$species_exp)
+which(isomex_250$species_exp=="green - white")
+head(isomex_250[718,]$genus_exp)
+isomex_250[c(718, 719, 720),]$species_exp="unidentifiable"
+
+# subsets to work with
+isomex_first<-isomex_250 %>% 
+  group_by(sampleID) %>% 
+  slice_head()
+isomex_first<-as.data.frame(isomex_first)
+
+isomex_250_scores<-
+  isomex_250%>% 
+  filter(!is.na(score)) %>% 
+  arrange(desc(score)) %>% 
+  select(score,latin_name, species_exp, genus, 
+         genus_exp, family, family_exp, image_url)
